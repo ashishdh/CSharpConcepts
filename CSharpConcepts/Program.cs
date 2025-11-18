15b0d5a15e592178c0e8fc5a27c84af375919222
dhroliyaashish <dhroliya.ashish@gmail.com>
2025-11-18 20:11:51 +0530

    Add project files.

---
+++ CSharpConcepts/Program.cs
@@ -0,0 +1,13 @@
+﻿using CSharpConcepts.Classes;
+
+namespace CSharpConcepts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleClass simple = new SimpleClass();
+            simple.Calculate(); 
+        }
+    }
+}
