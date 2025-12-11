7fe2fb1490d6bf00730fdf6cff8bd20d8b5d5a1a
dhroliyaashish <dhroliya.ashish@gmail.com>
2025-12-11 20:52:30 +0530

    This KeyWord

--- OOPSConcepts/RunnerClass.cs
+++ OOPSConcepts/RunnerClass.cs
@@ -7,6 +7,12 @@ namespace OOPSConcepts
         static void Main(string[] args)
         {
             new ConstructuresExample().Run();
+
+            // This Keyword Example
+            ThisKeyword obj = new ThisKeyword();
+            obj.SetName("Ashish");      
+            string name = obj.GetName();         
+            Console.WriteLine("Name using THIS keyword: " + name);
         }
     }
 }
---
+++ OOPSConcepts/ThisKeyword.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSConcepts
+{
+    internal class ThisKeyword
+    {
+        public string Name;
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetName(string Name)
+        {
+            this.Name = Name;
+        }        
+    }
+}
+
